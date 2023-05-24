f8e90acadd8becaf694f3310d90fdcc9dded21f8
yeeun <yeeun9507@gmail.com>
2023-05-24 15:48:27 +0900

    2023_05_24_VUE_TODOLIST

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,13 +1,59 @@
 <template>
-  <div>input</div>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="Type what u have to do" v-on:keyup.enter="addTodo">
+    <!-- <button v-on:click="addTodo">추가</button> -->
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fa fa-plus"></i> <!-- fa fa-plus "+"아이콘 추가 -->
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      newTodoItem : '' // input의 값이 입력되면 newTodoItem의 값도 같이 갱신
+    }
+  },
+  methods: {
+    addTodo() {
+      //console.log(this.newTodoItem);
+      if(this.newTodoItem != ''){ // 1. input박스 값이 있을시에만 저장
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        //localStorage.setItem(value,value); // setItem(키,값)로컬스토리지에 데이터를 추가하는 API
+        this.$emit('addTodo',value); // App.vue에 있는  localStorage.setItem()
+        this.clearInput(); // 2. 스토리지에 저장된후 input 박스 입력값 초기화
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
 <style>
-
+  input:focus {
+    outline: none;
+  }
+  .inputBox { 
+    background:white;
+    height: 50px;
+    line-height: 50px; /* text중앙정렬 */
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
 </style>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,13 +1,59 @@
 <template>
-  <div>list</div>
+  <section>
+    <transition-group name="list" tag="ul">
+        <li v-for="todoItem, index in propsdata" :key="index" class="shadow"> <!-- v-bind:key="todoItem" primaryKey 지정해준것 안그럼 오류 -->
+          <i class="checkBtn fa fa-check" aria-hidden="true"></i><!-- 체크아이콘 -->
+          {{ todoItem }}
+          <span class="removeBtn" type="button" v-on:click="removeTodo(todoItem,index)">
+            <i class="far fa-trash-alt" aria-hidden="true"></i><!-- 삭제아이콘 -->
+          </span>
+        </li> 
+    </transition-group>
+  </section>
 </template>
 
 <script>
-export default {
-
+export default { 
+  props : ['propsdata'],
+  methods: {
+    removeTodo(todoItem,index) {
+      //console.log(todoItem,index);
+      this.$emit('removeTodo',todoItem,index);
+    }
+  }
 }
 </script>
 
 <style>
-
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translate(30px);
+  }
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    background: white;
+    border-radius: 5px;
+    }
+    .checkBtn {
+      line-height: 45px;
+      color:#62acde;
+      margin-right: 5px;
+    }
+    .removeBtn {
+      margin-left: auto;
+      color: #de4343;
+    }
 </style>
