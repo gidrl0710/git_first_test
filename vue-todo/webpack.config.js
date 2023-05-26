d17411b2b4e5e333fa7a6c22252c15e6fbf3c372
yeeun <yeeun9507@gmail.com>
2023-05-26 18:58:40 +0900

    2023_03_26_VUE

--- vue-todo/webpack.config.js
+++ vue-todo/webpack.config.js
@@ -1,22 +1,31 @@
-var path = require('path')
+var path = require('path') 
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/main.js', // 웹펙으로 빌드할파일(이 안에 들어있는 내용들이 웹팩으로 번들링(빌드))
+  // output : 웹팩으로 빌드를 하고 난 결과물 파일의 위치와 이름지정
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
+
+  // module : 웹팩으로 파일들을 빌드 할 때 HTML,CSS,PNG 등의 파일을 자바스크립트로 변환해주는 로더 지정
   module: {
     rules: [
+      // 'css-loader'를 적용하여 css파일을 자바스크립트로 변환
+      // 변환된 css속성들이 최종적으로 'vue-style-loader'를 거쳐 index.html에 <style>로 삽입
       {
         test: /\.css$/,
         use: [
           'vue-style-loader',
           'css-loader'
         ],
-      },      {
+      },      
+
+      // vue 파일에 'vue-loader'적용
+      // vue 파일에 <style><script><template> 등의 내용이 자바스크립트로 변환되어 웹팩 빌드 결과물에 포함
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
@@ -25,11 +34,16 @@ module.exports = {
           // other vue-loader options go here
         }
       },
+
+      // js파일에 'babel-loader' 적용
+      // js파일의 ES6문법을 모든 브라우저에서 호환가능한 js로 변환해줌
       {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
       },
+
+      // 이미지 파일들을 'file-loader'를 이용하여 js파일로 변환
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
@@ -39,20 +53,28 @@ module.exports = {
       }
     ]
   },
+
+  // 웹팩으로 빌드시 뷰라이브러리의 유형선택
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      'vue$': 'vue/dist/vue.esm.js' // vue.esm.js : 최신 웹팩 버전과 사용할 수 있는 full버전의 뷰라이브러리
     },
     extensions: ['*', '.js', '.vue', '.json']
   },
+
+  // 웹팩 데브서버 관련 속성 지정
   devServer: {
     historyApiFallback: true,
     noInfo: true,
-    overlay: true
+    overlay: true // 웹팩으로 빌드시 오류 존재하면 브라우저 화면 전체에 오류 표시
   },
+
+  // 웹팩으로 빌드한 파일의 크키가 250kb넘으면 경고표시 할지 설정
   performance: {
     hints: false
   },
+
+  // 웹팩으로 빌드 된 내용 구동시 개발자도구에서 사용 할 디버깅 방식지정
   devtool: '#eval-source-map'
 }
 
